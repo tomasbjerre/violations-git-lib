4fc505b43745155d19c6f1ac5deddb3a04d52ed4
Tomas Bjerre <tomas.bjerre85@gmail.com>
2021-05-01 09:03:09 +0200

    more logging when checking diff

--- src/main/java/se/bjurr/violations/git/ViolationsGit.java
+++ src/main/java/se/bjurr/violations/git/ViolationsGit.java
@@ -21,11 +21,14 @@ public class ViolationsGit {
 
   public Set<Violation> getViolationsInChangeset(
       final File file, final String from, final String to) throws Exception {
+    this.logger.log(
+        Level.INFO,
+        "Getting violations between " + from + " and " + to + " in " + file.getAbsolutePath());
     final DiffsPerFile diffs = ViolationsGitRepo.diff(file, from, to);
     final Set<Violation> filtered = new TreeSet<>();
     for (final Violation candidate : this.violations) {
       final Optional<String> patchStringOpt = diffs.findPatchString(candidate.getFile());
-      this.logger.log(Level.FINE, "Checking if candidate " + candidate.getFile() + " is in diff");
+      this.logger.log(Level.INFO, "Checking if candidate " + candidate.getFile() + " is in diff");
       if (patchStringOpt.isPresent()) {
         final String patchString = patchStringOpt.get();
         final Integer violatedLine = candidate.getStartLine();
@@ -33,7 +36,7 @@ public class ViolationsGit {
           filtered.add(candidate);
         } else {
           this.logger.log(
-              Level.FINE,
+              Level.INFO,
               "Violated line ("
                   + violatedLine
                   + ") in "
@@ -42,7 +45,7 @@ public class ViolationsGit {
                   + patchString);
         }
       } else {
-        this.logger.log(Level.FINE, "Candidate not found in diff: " + candidate.getFile());
+        this.logger.log(Level.INFO, "Candidate not found in diff: " + candidate.getFile());
       }
     }
     return filtered;
